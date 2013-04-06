6fd1657d827a95f80c9ff837411ddf558ccd2b2f
kumezaki <kumezaki@uci.edu>
2013-04-05 21:41:16 -0700

    Adding Xcode Single View Application files.

---
+++ Music147_2013/Music147_2013/MUS147ViewController.h
@@ -0,0 +1,13 @@
+//
+//  MUS147ViewController.h
+//  Music147_2013
+//
+//  Created by Kojiro Umezaki on 4/5/13.
+//  Copyright (c) 2013 Kojiro Umezaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUS147ViewController : UIViewController
+
+@end
---
+++ Music147_2013/Music147_2013/MUS147ViewController.m
@@ -0,0 +1,29 @@
+//
+//  MUS147ViewController.m
+//  Music147_2013
+//
+//  Created by Kojiro Umezaki on 4/5/13.
+//  Copyright (c) 2013 Kojiro Umezaki. All rights reserved.
+//
+
+#import "MUS147ViewController.h"
+
+@interface MUS147ViewController ()
+
+@end
+
+@implementation MUS147ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
