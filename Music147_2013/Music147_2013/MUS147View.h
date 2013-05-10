b218a3688dfb3998447ad035d9c178beef66f23c
kumezaki <kumezaki@uci.edu>
2013-05-10 09:18:59 -0400

    Somehow the MUS147View and .xib files were missing from the previous commit.  This commit adds them back.

---
+++ Music147_2013/Music147_2013/MUS147View.h
@@ -0,0 +1,18 @@
+//
+//  MUS147View.h
+//  Music147_2013
+//
+//  Created by Kojiro Umezaki on 5/3/13.
+//  Copyright (c) 2013 Kojiro Umezaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUS147View : UIView {
+    UITouch* touch;
+}
+
+-(void)doTouchesOn:(NSSet *)touches withEvent:(UIEvent *)event;
+-(void)doTouchesOff:(NSSet *)touches withEvent:(UIEvent *)event;
+
+@end
